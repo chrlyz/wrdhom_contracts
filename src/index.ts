9edea0d237c11eafb5f3577d9b07b540b0e54be5
chrlyz <chrlyz@skiff.com>
2024-01-04 02:33:39 +0000

    Release 0.2.0 package version

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
-import { PostState, PostsTransition, Posts, PostsProof } from './posts/Posts.js';
+import {
+  PostState,
+  PostsTransition,
+  Posts,
+  PostsProof,
+} from './posts/Posts.js';
 import { PostsContract } from './posts/PostsContract.js';
-import { RepostState, RepostsTransition, Reposts } from './reposts/Reposts.js';
+import {
+  RepostState,
+  RepostsTransition,
+  Reposts,
+  RepostsProof,
+} from './reposts/Reposts.js';
 import { RepostsContract } from './reposts/RepostsContract.js';
+import {
+  ReactionState,
+  ReactionsTransition,
+  Reactions,
+} from './reactions/Reactions.js';
+import { ReactionsContract } from './reactions/ReactionsContract.js';
 import { Config } from './posts/PostsDeploy.js';
 
 export {
   PostState,
   PostsTransition,
   Posts,
+  PostsProof,
   PostsContract,
   RepostState,
   RepostsTransition,
   Reposts,
+  RepostsProof,
   RepostsContract,
+  ReactionState,
+  ReactionsTransition,
+  Reactions,
+  ReactionsContract,
   Config,
-  PostsProof,
 };
