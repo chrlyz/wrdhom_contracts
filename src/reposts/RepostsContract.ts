7d0eb9a85f1682a1481d73182be09f029537ce51
chrlyz <chrlyzxchrlyz@gmail.com>
2024-12-05 05:08:05 +0000

    Implement provable state history for reposts zkApp

--- src/reposts/RepostsContract.ts
+++ src/reposts/RepostsContract.ts
@@ -1,4 +1,14 @@
-import { Field, SmartContract, state, State, method, UInt32, Gadgets } from 'o1js';
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  UInt32,
+  Gadgets,
+  Poseidon,
+  MerkleMapWitness
+} from 'o1js';
 import { RepostsProof } from './Reposts.js';
 import {
   PostsContract,
@@ -13,6 +23,8 @@ export class RepostsContract extends SmartContract {
   @state(Field) usersRepostsCounters = State<Field>();
   @state(Field) targetsRepostsCounters = State<Field>();
   @state(Field) reposts = State<Field>();
+  @state(Field) lastUpdate = State<Field>();
+  @state(Field) stateHistory = State<Field>();
 
   init() {
     super.init();
@@ -20,49 +32,61 @@ export class RepostsContract extends SmartContract {
     this.usersRepostsCounters.set(newMerkleMapRoot);
     this.targetsRepostsCounters.set(newMerkleMapRoot);
     this.reposts.set(newMerkleMapRoot);
+    this.lastUpdate.set(Field(0));
+    this.stateHistory.set(newMerkleMapRoot);
   }
 
-  @method async update(proof: RepostsProof) {
+  @method async update(proof: RepostsProof, stateHistoryWitness: MerkleMapWitness) {
     proof.verify();
-    Gadgets.rangeCheck32(proof.publicInput.blockHeight);
 
-    this.network.blockchainLength.requireBetween(
-      UInt32.Unsafe.fromField(proof.publicInput.blockHeight),
-      UInt32.Unsafe.fromField(proof.publicInput.blockHeight).add(10)
-    );
+    Gadgets.rangeCheck32(proof.publicInput.blockHeight);
+    const blockHeightAsField = proof.publicInput.blockHeight;
+    const blockHeight = UInt32.Unsafe.fromField(proof.publicInput.blockHeight);
+    this.network.blockchainLength.requireBetween(blockHeight,blockHeight.add(10));
 
     const postsContract = new PostsContract(postsContractAddress);
     const currentPosts = postsContract.posts.getAndRequireEquals();
     proof.publicInput.targets.assertEquals(currentPosts);
 
-    const currentAllRepostsCounter =
-      this.allRepostsCounter.getAndRequireEquals();
-    proof.publicInput.initialAllRepostsCounter.assertEquals(
-      currentAllRepostsCounter
-    );
+    const currentAllRepostsCounter = this.allRepostsCounter.getAndRequireEquals();
+    const initialAllRepostsCounter = proof.publicInput.initialAllRepostsCounter;
+    currentAllRepostsCounter.assertEquals(initialAllRepostsCounter);
 
-    const currentUsersRepostsCounters =
-      this.usersRepostsCounters.getAndRequireEquals();
-    proof.publicInput.initialUsersRepostsCounters.assertEquals(
-      currentUsersRepostsCounters
-    );
+    const currentUsersRepostsCounters = this.usersRepostsCounters.getAndRequireEquals();
+    const initialUsersRepostsCounters = proof.publicInput.initialUsersRepostsCounters;
+    currentUsersRepostsCounters.assertEquals(initialUsersRepostsCounters);
 
-    const currentTargetsRepostsCounters =
-      this.targetsRepostsCounters.getAndRequireEquals();
-    proof.publicInput.initialTargetsRepostsCounters.assertEquals(
-      currentTargetsRepostsCounters
-    );
+    const currentTargetsRepostsCounters = this.targetsRepostsCounters.getAndRequireEquals();
+    const initialTargetsRepostsCounters = proof.publicInput.initialTargetsRepostsCounters;
+    currentTargetsRepostsCounters.assertEquals(initialTargetsRepostsCounters);
 
     const currentReposts = this.reposts.getAndRequireEquals();
-    proof.publicInput.initialReposts.assertEquals(currentReposts);
+    const initialReposts = proof.publicInput.initialReposts;
+    currentReposts.assertEquals(initialReposts);
 
-    this.allRepostsCounter.set(proof.publicInput.latestAllRepostsCounter);
-    this.usersRepostsCounters.set(
-      proof.publicInput.latestUsersRepostsCounters
-    );
-    this.targetsRepostsCounters.set(
-      proof.publicInput.latestTargetsRepostsCounters
-    );
-    this.reposts.set(proof.publicInput.latestReposts);
+    const currentStateHistory = this.stateHistory.getAndRequireEquals();
+    const [initialStateHistory, stateHistoryKey] = stateHistoryWitness.computeRootAndKeyV2(Field(0));
+    currentStateHistory.assertEquals(initialStateHistory);
+    blockHeightAsField.assertEquals(stateHistoryKey);
+
+    const latestAllRepostsCounter = proof.publicInput.latestAllRepostsCounter;
+    const latestUsersRepostsCounters = proof.publicInput.latestUsersRepostsCounters;
+    const latestTargetsRepostsCounters = proof.publicInput.latestTargetsRepostsCounters;
+    const latestReposts = proof.publicInput.latestReposts;
+
+    const latestState = Poseidon.hash([
+      latestAllRepostsCounter,
+      latestUsersRepostsCounters,
+      latestTargetsRepostsCounters,
+      latestReposts
+    ]);
+    const latestStateHistory = stateHistoryWitness.computeRootAndKeyV2(latestState)[0];
+
+    this.allRepostsCounter.set(latestAllRepostsCounter);
+    this.usersRepostsCounters.set(latestUsersRepostsCounters);
+    this.targetsRepostsCounters.set(latestTargetsRepostsCounters);
+    this.reposts.set(latestReposts);
+    this.lastUpdate.set(blockHeightAsField);
+    this.stateHistory.set(latestStateHistory);
   }
 }
